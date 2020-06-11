83a88218b33749361e38b644a25e9b9f2b6de6d0
Павел Туча <p.tucha@krtech.ru>
2020-06-11 21:27:06 +0300

    Add favicon, bootstrap webjar styles, sample css and request parameter

--- src/main/java/ru/tuchanet/sftpclient/controller/SftpController.java
+++ src/main/java/ru/tuchanet/sftpclient/controller/SftpController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ru.tuchanet.sftpclient.service.SftpService;
 
@@ -14,9 +15,10 @@ public class SftpController {
 	SftpService sftpService;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
+	public String list(Model model, @RequestParam(name = "dir", required = false, defaultValue = "") String dir) {
 		
 		model.addAttribute("svc", sftpService.toString());
+		model.addAttribute("dir", dir);
 		
 		return "sftp/list";
 	}
