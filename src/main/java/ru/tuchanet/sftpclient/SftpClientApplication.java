92d257cca2fc8392a35c400604bcd9758045bd11
Павел Туча <p.tucha@krtech.ru>
2020-06-16 14:11:27 +0300

    Поправлена локализация

--- src/main/java/ru/tuchanet/sftpclient/SftpClientApplication.java
+++ src/main/java/ru/tuchanet/sftpclient/SftpClientApplication.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -15,11 +16,20 @@ public class SftpClientApplication {
 		SpringApplication.run(SftpClientApplication.class, args);
 	}
 	
+	
+	@Bean(name = "messageSource")
+	public ResourceBundleMessageSource getMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages/messages");
+	    messageSource.setDefaultEncoding("UTF-8");
+	    return messageSource;
+	}
+	
 	@Bean
 	public LocaleResolver localeResolver() {
 	    SessionLocaleResolver slr = new SessionLocaleResolver();
 	    slr.setDefaultLocale(Locale.forLanguageTag("ru"));
 	    return (LocaleResolver) slr;
-	}	
+	}
 	
 }
