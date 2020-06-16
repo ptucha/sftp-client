9441a09f0fb6e3b4ab32b51205967dc94d6446a5
Павел Туча <p.tucha@krtech.ru>
2020-06-16 11:20:07 +0300

    Multilanguage support

--- src/main/java/ru/tuchanet/sftpclient/SftpClientApplication.java
+++ src/main/java/ru/tuchanet/sftpclient/SftpClientApplication.java
@@ -1,7 +1,12 @@
 package ru.tuchanet.sftpclient;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SftpClientApplication {
@@ -9,5 +14,12 @@ public class SftpClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SftpClientApplication.class, args);
 	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.forLanguageTag("ru"));
+	    return (LocaleResolver) slr;
+	}	
 	
 }
